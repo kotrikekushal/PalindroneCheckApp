aad4f4487f80ece34af55fbf7646330fb994133e
kotrikekushal <kotrikekushal@gmail.com>
2026-03-10 00:30:41 +0530

    UC11 Object Oriented Palindrome Service

--- App/src/PalindromeCheckApp.java
+++ App/src/PalindromeCheckApp.java
@@ -1,33 +1,40 @@
 import java.util.Scanner;
+import java.util.Stack;
+
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String word) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
 
 public class PalindromeCheckApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a sentence: ");
-        String input = scanner.nextLine();
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
 
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        PalindromeChecker checker = new PalindromeChecker();
 
-        boolean isPalindrome = true;
-
-        int left = 0;
-        int right = normalized.length() - 1;
-
-        while (left < right) {
-            if (normalized.charAt(left) != normalized.charAt(right)) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
-        }
-
-        if (isPalindrome) {
-            System.out.println("Palindrome (ignoring spaces and case)");
+        if (checker.checkPalindrome(word)) {
+            System.out.println(word + " is a Palindrome");
         } else {
-            System.out.println("Not a Palindrome");
+            System.out.println(word + " is NOT a Palindrome");
         }
 
         scanner.close();
